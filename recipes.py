90a189c57cd74dc44db70100edb11f3f61dbd7a1
Rodion <rodikfeed@gmail.com>
2023-11-02 02:42:11 +0300

    task-1

---
+++ recipes.py
@@ -0,0 +1,22 @@
+cook_book = {
+    'Овсяная каша': [
+        {'ingredient_name': 'Овсянка', 'quantity': 70, 'measure': 'гр.'},
+        {'ingredient_name': 'Молоко', 'quantity': 50, 'measure': 'мл'},
+        {'ingredient_name': 'Вода', 'quantity': 50, 'measure': 'мл'}
+        ],
+    'Стейк из говядины': [
+        {'ingredient_name': 'Кусок говядины', 'quantity': 1, 'measure': 'шт'},
+        {'ingredient_name': 'Масло', 'quantity': 10, 'measure': 'мл'},
+        {'ingredient_name': 'Приправы', 'quantity': 5, 'measure': 'гр'},
+        ],
+    'Лапша с морепродуктами': [
+        {'ingredient_name': 'Морепродукты', 'quantity': 300, 'measure': 'гр'},
+        {'ingredient_name': 'Лапша', 'quantity': 70, 'measure': 'гр'},
+        {'ingredient_name': 'Вода', 'quantity': 1, 'measure': 'л'},
+        ],
+    'Топор': [
+        {'ingredient_name': 'Деревянная рукоять', 'quantity': 1, 'measure': 'шт'},
+        {'ingredient_name': 'Железное окончание', 'quantity': 1, 'measure': 'шт'},
+        {'ingredient_name': 'Клей', 'quantity': 1, 'measure': 'гр'},
+        ]
+    }
